111f235ea7ae9d1a8307cfc33c8002b649ff93d4
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-16 08:54:31 +0300

    working with sql

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3003
-const mysql = require('mysql2')
-require('dotenv').config();
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = 3003;
+const mysql = require("mysql2");
+require("dotenv").config();
+const cors = require("cors");
 
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
-app.get('/',(req,res)=>{
-    res.send('hey')
-})
+app.get("/", (req, res) => {
+  res.send("hey");
+});
 
-app.get('/allbooks', (req,res)=>{
-    const q = 'SELECT * FROM books'
-    db.query(q, (err,data)=>{
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
+app.get("/allbooks", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
-app.post('/bookdetails', (req,res)=>{
+app.post("/bookdetails", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
 
-    const q = 'INSERT INTO books(`title`, `desc`, `cover`) VALUES (?)'
-    const values = [req.body.title, req.body.desc, req.body.cover]
+  db.query(q, [values], (err, data) => {
+    if (err) return res.status(500).json({ err });
+    return res.status(201).json({ message: "Book has been created", values });
+  });
+});
 
-    db.query(q,[values],(err,data)=>{
-        if(err) return res.status(500).json({err})
-        return res.status(201).json({message:'Book has been created',values})
-
-    })
-
-})
-
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
